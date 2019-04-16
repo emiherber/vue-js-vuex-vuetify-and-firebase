98820425629bb134cfdcd9453762c6bcaed42ba7
emiherber <emiherbes@hotmail.com>
2019-04-15 23:01:32 -0300

    Using Vuex and accessing API

--- src/store.js
+++ src/store.js
@@ -1,10 +1,35 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import Axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+    state: {
+        recipes: [],
+        apiUrl: "https://api.edamam.com/search"
+    },
+    mutations: {
+        setRecipes(state, payload) {
+            state.recipes = payload;
+        }
+    },
+    actions: {
+        async getRecipes({ state, commit }, plan) {
+            try {
+                let response = await Axios.get(`${state.apiUrl}`, {
+                    params: {
+                        q: plan,
+                        app_id: "<tu_id>",
+                        app_key: "<tu_key>",
+                        from: 0,
+                        to: 9
+                    }
+                });
+                commit("setRecipes", response.data.hits);
+            } catch (error) {
+                commit("setRecipes", []);
+            }
+        }
+    }
 });
